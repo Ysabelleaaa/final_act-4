efe96f73b893e20cb1c5efef674fcc363818b014
Jessa Mae N. Gadot <jmgadot@paterostechnologicalcollege.edu.ph>
2026-05-19 21:44:46 +0800

    Add voting eligibility form and leap year check

---
+++ lab-act #8.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Voting Eligibility</h2>
+
+<form method="post">
+    Enter a year: <input type="number" name="yr" required> <br><br>
+    <input type="submit" value="Submit"> <br><br>
+</form>
+
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$yr = $_POST["yr"];
+
+if(($yr % 400 == 0) || ($yr % 4 == 0 && $yr % 100 != 0)) {
+    echo "Leap Year";
+} else {
+    echo "Not a Leap Year";
+}
+}
+?>
+
+</body>
+</html>
